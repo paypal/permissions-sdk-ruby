83b9f982d5446b1a9b34a165d7df7c174eb21f4b
siddick <mebramsha@paypal.com>
2012-11-22 12:02:26 +0530

    Set default callback url & handle return params from paypal

--- samples/app/controllers/permissions_samples/permissions_controller.rb
+++ samples/app/controllers/permissions_samples/permissions_controller.rb
@@ -10,12 +10,19 @@ module PermissionsSamples
 
     def request_permissions
       @request_permissions = api.build_request_permissions(params[:RequestPermissionsRequest] || default_api_value)
-      @request_permissions_response = api.request_permissions(@api_request) if request.post?
+      # Set default callback URL
+      @request_permissions.callback ||= permissions_url(:get_access_token)
+      @request_permissions_response = api.request_permissions(@request_permissions) if request.post?
     end
 
     def get_access_token
       @get_access_token = api.build_get_access_token(params[:GetAccessTokenRequest] || default_api_value)
-      @get_access_token_response = api.get_access_token(@api_request) if request.post?
+
+      # To handle the return value from paypal
+      @get_access_token.token     = params["request_token"]     if params["request_token"]
+      @get_access_token.verifier  = params["verification_code"] if params["verification_code"]
+
+      @get_access_token_response = api.get_access_token(@get_access_token) if request.post?
     end
 
     private
